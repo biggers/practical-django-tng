25e8dce04eb89a6361479ce42d915fffe3b6e92c
James Bennett <james@b-list.org>
2009-07-07 07:02:51 -0500

    Add redirect when only one keyword result matches (pp. 39-40).

--- cms/search/views.py
+++ cms/search/views.py
@@ -1,13 +1,15 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.contrib.flatpages.models import FlatPage
 
 
 def search(request):
     query = request.GET.get('q', '')
-    keyword_results = []
-    results = []
+    keyword_results = results = []
     if query:
         keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in=query.split()).distinct()
+        if keyword_results.count() == 1:
+            return HttpResponseRedirect(keyword_results[0].get_absolute_url())
         results = FlatPage.objects.filter(content__icontains=query)
     return render_to_response('search/search.html',
                               { 'query': query,
