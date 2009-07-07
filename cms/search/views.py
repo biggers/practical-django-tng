13093f0f1650063b175a977b305ce6d2919103f9
James Bennett <james@b-list.org>
2009-07-07 04:58:42 -0500

    Edit search view to use render_to_response (page 31).

--- cms/search/views.py
+++ cms/search/views.py
@@ -1,12 +1,9 @@
-from django.http import HttpResponse
-from django.template import loader, Context
+from django.shortcuts import render_to_response
 from django.contrib.flatpages.models import FlatPage
 
 
 def search(request):
     query = request.GET['q']
-    results = FlatPage.objects.filter(content__icontains=query)
-    template = loader.get_template('search/search.html')
-    context = Context({ 'query': query, 'results': results })
-    response = template.render(context)
-    return HttpResponse(response)
+    return render_to_response('search/search.html',
+                              { 'query': query,
+                                'results': FlatPage.objects.filter(content__icontains=query) })
