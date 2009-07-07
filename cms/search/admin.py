d94e515cae89b41ccf0ac64cde46f19f7ecd11cf
James Bennett <james@b-list.org>
2009-07-07 06:54:44 -0500

    Change search keywords to be edited inline with flatpages (pp. 36-37).

--- cms/search/admin.py
+++ cms/search/admin.py
@@ -1,10 +1,17 @@
 from django.contrib import admin
+from django.contrib.flatpages.admin import FlatPageAdmin
+from django.contrib.flatpages.models import FlatPage
 
 from cms.search.models import SearchKeyword
 
 
-class SearchKeywordAdmin(admin.ModelAdmin):
-    pass
+class SearchKeywordInline(admin.StackedInline):
+    model = SearchKeyword
 
 
-admin.site.register(SearchKeyword, SearchKeywordAdmin)
+class FlatPageAdminWithKeywords(FlatPageAdmin):
+    inlines = [SearchKeywordInline]
+
+
+admin.site.unregister(FlatPage)
+admin.site.register(FlatPage, FlatPageAdminWithKeywords)
