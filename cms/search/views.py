045c9966b94b1db9f65474fce3a4ce1c5bfb6aa0
James Bennett <james@b-list.org>
2009-07-07 06:57:15 -0500

    Add keyword support to search view (page 38).

--- cms/search/views.py
+++ cms/search/views.py
@@ -4,9 +4,12 @@ from django.contrib.flatpages.models import FlatPage
 
 def search(request):
     query = request.GET.get('q', '')
+    keyword_results = []
     results = []
     if query:
+        keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in=query.split()).distinct()
         results = FlatPage.objects.filter(content__icontains=query)
     return render_to_response('search/search.html',
                               { 'query': query,
+                                'keyword_results': keyword_results,
                                 'results': results })
