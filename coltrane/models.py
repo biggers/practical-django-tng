10cbb00483a253e6544b254d5ba055810ce17c88
James Bennett <james@b-list.org>
2009-09-17 05:50:12 -0500

    Add choice for hidden status (p. 58).

--- coltrane/models.py
+++ coltrane/models.py
@@ -24,9 +24,11 @@ class Category(models.Model):
 class Entry(models.Model):
     LIVE_STATUS = 1
     DRAFT_STATUS = 2
+    HIDDEN_STATUS = 3
     STATUS_CHOICES = (
         (LIVE_STATUS, 'Live'),
         (DRAFT_STATUS, 'Draft'),
+        (HIDDEN_STATUS, 'Hidden'),
     )
     title = models.CharField(max_length=250)
     excerpt = models.TextField(blank=True)
