3df5746e02c3519e14cf8ff310358f3c016486fd
James Bennett <james@b-list.org>
2009-09-17 04:46:19 -0500

    Add date-based unique constraint to the slug field of the Entry model (p. 54).

--- coltrane/models.py
+++ coltrane/models.py
@@ -22,4 +22,4 @@ class Entry(models.Model):
     excerpt = models.TextField(blank=True)
     body = models.TextField()
     pub_date = models.DateTimeField()
-    slug = models.SlugField()
+    slug = models.SlugField(unique_for_date='pub_date')
