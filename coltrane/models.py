c67395828458147735e2c6512732a16db9bb76d3
James Bennett <james@b-list.org>
2009-09-17 04:42:38 -0500

    Begin defining the Entry model (p. 53).

--- coltrane/models.py
+++ coltrane/models.py
@@ -15,3 +15,10 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return "/categories/%s/" % self.slug
+
+
+class Entry(models.Model):
+    title = models.CharField(max_length=250)
+    excerpt = models.TextField(blank=True)
+    body = models.TextField()
+    pub_date = models.DateTimeField()
