86fed1982251852a009e34945c5130ff7ee3a04e
James Bennett <james@b-list.org>
2009-09-17 04:37:53 -0500

    Add help text for category slugs (p. 51). ERRATA NOTICE: The unique constraint for this field was left out of the listing on p. 51 of the book.

--- coltrane/models.py
+++ coltrane/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=250, help_text='Maximum 250 characters.')
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, help_text="Suggested value automatically generated from title. Must be unique.")
     description = models.TextField()
 
     class Meta:
