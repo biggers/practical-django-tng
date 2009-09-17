5b06a57ccc53794dc3aaa45799b3cee7c712cfbf
James Bennett <james@b-list.org>
2009-09-17 04:25:40 -0500

    Start writing the Category model (p. 47).

---
+++ coltrane/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.title
