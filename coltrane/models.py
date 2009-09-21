c06f63728f107802b327cf5c04bae476c46b7cde
James Bennett <james@b-list.org>
2009-09-21 07:50:23 -0500

    Use named URLs and the permalink decorator for the get_absolute_url method on Entry (p. 74).

--- coltrane/models.py
+++ coltrane/models.py
@@ -74,5 +74,8 @@ class Entry(models.Model):
         super(Entry, self).save(force_insert, force_update)
 
     def get_absolute_url(self):
-        return "/weblog/%s/%s/" % (self.pub_date.strftime("%Y/%b/%d").lower(),
-                                   self.slug)
+        return ('coltrane_entry_detail', (), { 'year': self.pub_date.strftime("%Y"),
+                                               'month': self.pub_date.strftime("%b").lower(),
+                                               'day': self.pub_date.strftime("%d"),
+                                               'slug': self.slug })
+    get_absolute_url = models.permalink(get_absolute_url)
