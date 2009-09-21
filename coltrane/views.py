0fe2062583d75516e19550e055d182bfafd483c2
James Bennett <james@b-list.org>
2009-09-21 05:03:00 -0500

    Use get_object_or_404 in entry_detail view (pp. 68-69).

--- coltrane/views.py
+++ coltrane/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404, render_to_response
 from coltrane.models import Entry
 
 
@@ -11,7 +11,7 @@ def entry_detail(request, year, month, day, slug):
     date_stamp = time.strptime(year+month+day, "%Y%b%d")
     pub_date = datetime.date(*date_stamp[:3])
     return render_to_response('coltrane/entry_detail.html',
-                              { 'entry': Entry.objects.get(pub_date__year=pub_date.year,
+                              { 'entry': get_object_or_404(Entry, pub_date__year=pub_date.year,
                                                            pub_date__month=pub_date.month,
                                                            pub_date__day=pub_date.day,
                                                            slug=slug) })
