785308540dd5ec348a3e07b0a12972aec2fef81f
James Bennett <james@b-list.org>
2009-10-19 04:49:39 -0500

    Add category detail view (p. 85).

--- coltrane/views.py
+++ coltrane/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import get_object_or_404, render_to_response
 
 from coltrane.models import Category
 
@@ -6,3 +6,9 @@ from coltrane.models import Category
 def category_list(request):
     return render_to_response('coltrane/category_list.html',
                               { 'object_list': Category.objects.all() })
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    return render_to_response('coltrane/category_detail.html',
+                              { 'object_list': category.entry_set.all(),
+                                'category': category })
