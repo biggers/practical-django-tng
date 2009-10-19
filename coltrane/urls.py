1e8838c66d2f391fc51f6ecf06c7506e53ce80de
James Bennett <james@b-list.org>
2009-10-19 03:57:24 -0500

    Add URL patterns for links (p. 83).

--- coltrane/urls.py
+++ coltrane/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls.defaults import *
 
-from coltrane.models import Entry
+from coltrane.models import Entry, Link
 
 entry_info_dict = {
     'queryset': Entry.objects.all(),
+    'date_field': 'pub_date',
+}
+
+link_info_dict = {
+    'queryset': Link.objects.all(),
     'date_field': 'pub_date',
 }
 
@@ -29,4 +34,24 @@ urlpatterns = patterns('django.views.generic.date_based',
      'object_detail',
      entry_info_dict,
      'coltrane_entry_detail'),
+    (r'^links/$',
+     'archive_index',
+     link_info_dict,
+     'coltrane_link_archive_index'),
+    (r'^links/(?P<year>\d{4})/$',
+     'archive_year',
+     link_info_dict,
+     'coltrane_link_archive_year'),
+    (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/$',
+     'archive_month',
+     link_info_dict,
+     'coltrane_link_archive_month'),
+    (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/$',
+     'archive_day',
+     link_info_dict,
+     'coltrane_link_archive_day'),
+    (r'^links/(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$',
+     'object_detail',
+     link_info_dict,
+     'coltrane_link_detail'),
 )
