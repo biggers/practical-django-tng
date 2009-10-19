986620f7a5dc1535430c224597b608d699b82f25
James Bennett <james@b-list.org>
2009-10-19 04:56:23 -0500

    Use wrapped generic view for category detail (p. 87).

--- coltrane/views.py
+++ coltrane/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404
+from django.views.generic.list_detail import object_list
 
 from coltrane.models import Category
 
 
 def category_detail(request, slug):
     category = get_object_or_404(Category, slug=slug)
-    return render_to_response('coltrane/category_detail.html',
-                              { 'object_list': category.entry_set.all(),
-                                'category': category })
+    return object_list(request, queryset=category.entry_set.all(),
+                       extra_context={ 'category': category })
