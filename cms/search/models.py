df2afb7b091cae64c1894436f8d8b2e0d0c91562
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-08 16:59:36 +0530

    To remove the default flatpage section

--- cms/search/models.py
+++ cms/search/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.flatpages.models import FlatPage
+#from django.contrib.flatpages.models import FlatPage
 
 
 class SearchKeyword(models.Model):
     keyword = models.CharField(max_length=50)
-    page = models.ForeignKey(FlatPage)
+#    page = models.ForeignKey(FlatPage)
 
     def __unicode__(self):
         return self.keyword
