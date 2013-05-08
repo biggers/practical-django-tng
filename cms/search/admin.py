50b37d78a2fca02f5186ea616d9ae40e364a1b82
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-08 16:53:52 +0530

    removed the default flatpage and added new flatpage-plus in installed_app

--- cms/search/admin.py
+++ cms/search/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from django.contrib.flatpages.admin import FlatPageAdmin
-from django.contrib.flatpages.models import FlatPage
+#from django.contrib.flatpages.admin import FlatPageAdmin
+#from django.contrib.flatpages.models import FlatPage
 
 from cms.search.models import SearchKeyword
 
@@ -9,9 +9,9 @@ class SearchKeywordInline(admin.StackedInline):
     model = SearchKeyword
 
 
-class FlatPageAdminWithKeywords(FlatPageAdmin):
-    inlines = [SearchKeywordInline]
+#class FlatPageAdminWithKeywords(FlatPageAdmin):
+#    inlines = [SearchKeywordInline]
 
 
-admin.site.unregister(FlatPage)
-admin.site.register(FlatPage, FlatPageAdminWithKeywords)
+#admin.site.unregister(FlatPage)
+#admin.site.register(FlatPage, FlatPageAdminWithKeywords)
