1deae9f878f57c36f561345a1e8ed445c655ec85
Shaiju Davis <shaiu.davis@vanillanetworks.com>
2013-05-28 14:27:24 +0530

    Updated search view file and base template

--- cms/search/views.py
+++ cms/search/views.py
@@ -1,17 +1,20 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
-from django.contrib.flatpages.models import FlatPage
+# for loading template
+from django.template import RequestContext
+#from django.contrib.flatpages.models import FlatPage
 
 
 def search(request):
     query = request.GET.get('q', '')
     keyword_results = results = []
     if query:
-        keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in=query.split()).distinct()
+#        keyword_results = FlatPage.objects.filter(searchkeyword__keyword__in=query.split()).distinct()
         if keyword_results.count() == 1:
             return HttpResponseRedirect(keyword_results[0].get_absolute_url())
-        results = FlatPage.objects.filter(content__icontains=query)
+#        results = FlatPage.objects.filter(content__icontains=query)'''
+
     return render_to_response('search/search.html',
                               { 'query': query,
                                 'keyword_results': keyword_results,
-                                'results': results })
+                                'results': results }, RequestContext(request),)
